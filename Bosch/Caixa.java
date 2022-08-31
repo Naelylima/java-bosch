303a6d3862659efa02d532249079f02f0289ed73
Naelylima <110494299+Naelylima@users.noreply.github.com>
2022-08-31 11:10:34 -0300

    Add files via upload
    Como procurar caractere, como contar, como comparar String, Caixa alta e baixa

---
+++ Bosch/Caixa.java
@@ -0,0 +1,11 @@
+package Bosch;
+
+public class Caixa {
+    public static void main(String[] args) {
+
+//      CAIXA ALTA
+//        System.out.println(minhaString.toUpperCase());
+//      CAIXA BAIXA
+//        System.out.println(minhaString.toLowerCase());
+    }
+}
