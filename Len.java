012d4eb869b2b2ce857e5f3a04ec8cbd5fbf6263
Naely Lima Pereira <110494299+Naelylima@users.noreply.github.com>
2022-09-12 12:07:32 -0300

    Add files via upload

---
+++ Len.java
@@ -0,0 +1,10 @@
+package Bosch;
+
+public class Len {
+    public static void main(String[] args) {
+
+//       CONTA OS CARACTERES TIPO 'LEN'
+//        System.out.println(minhaString.length());
+
+    }
+}
---
+++ aula3.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class aula3 {
+    public static void main(String[] args) {
+        Scanner
+    }
+}
