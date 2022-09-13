5c704aa1ea04a5b002e19f76c08c4d7dcbd3bc0f
Naely Lima Pereira <110494299+Naelylima@users.noreply.github.com>
2022-09-13 08:16:19 -0300

    Create ansOrNot.java

---
+++ ansOrNot.java
@@ -0,0 +1,12 @@
+// AND &&
+       boolean condicao1= true;
+        boolean condicao2=15<12;
+        System.out.println(condicao1 && condicao2);
+  // OR || (PAIPS)
+       boolean condicao1= false;
+       boolean condicao2=15<12;
+        System.out.println(condicao1 || condicao2);
+  // NOT !
+        boolean condicao1= true;
+        boolean condicao2=15<12;
+        System.out.println(!condicao1);
