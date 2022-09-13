4f87b1d1c49df6f7b9b3513f6fbe35fdf0d9f580
Naely Lima Pereira <110494299+Naelylima@users.noreply.github.com>
2022-09-13 08:12:43 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,8 +1,43 @@
 package Bosch;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+//        CONTATENAR
+//        String minhaString = "Like a bosch";
+//        System.out.println(minhaString + '!');
+//        System.out.println(minhaString.concat("!!!"));
+//        System.out.println(minhaString);
+//        minhaString = minhaString + '!';
+//        System.out.println(minhaString);
+//        minhaString = minhaString.concat("!!!");
+//        System.out.println(minhaString);
+
+//        Outro forma de definir variavel
+//        String minhaString = new String("Like a Bosch");
+//        System.out.println(minhaString);
+
+//       input do java
+//        String minhaString;
+//        Scanner entrada = new Scanner(System.in);
+//        minhaString = entrada.nextLine();
+
+//
+//  AND &&
+//        boolean condicao1= true;
+//        boolean condicao2=15<12;
+//        System.out.println(condicao1 && condicao2);
+//  OR || (PAIPS)
+//        boolean condicao1= false;
+//        boolean condicao2=15<12;
+//        System.out.println(condicao1 || condicao2);
+//   NOT !
+        boolean condicao1= true;
+        boolean condicao2=15<12;
+        System.out.println(!condicao1);
     }
 }
