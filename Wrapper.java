7f6c00775994bb73dfd84acef6e29bcd3f526083
Naely Lima Pereira <110494299+Naelylima@users.noreply.github.com>
2022-09-14 11:20:36 -0300

    Add files via upload

---
+++ Wrapper.java
@@ -0,0 +1,36 @@
+package Bosch;
+
+public class Wrapper {
+    public static void main(String[] args) {
+//        wrapper = classe
+
+//        Byte b = 100;
+//        Integer i = 10000;
+//        Short s= 100;
+//        Long l = 100000L;
+//
+//        System.out.println(b.byteValue());
+//        System.out.println(s.toString());
+//        System.out.println(i*3);
+//        System.out.println(l/3);
+//
+//        Float f = 123.10F;
+//        System.out.println(f);
+//
+//        Double d = 1234.5678;
+//        System.out.println(d);
+//
+//        Boolean bo = Boolean.parseBoolean("True");
+//        System.out.println(bo);
+//
+//        Character c = 'c';
+//        System.out.println(c);
+
+
+
+
+
+
+
+    }
+}
