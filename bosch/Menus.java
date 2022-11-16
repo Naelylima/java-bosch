9bd60f8218ac939e6c98de6d41374691b841c2c7
Naely Lima Pereira <110494299+Naelylima@users.noreply.github.com>
2022-11-16 11:17:39 -0300

    Update Menus.java

--- bosch/Menus.java
+++ bosch/Menus.java
@@ -21,6 +21,7 @@ public class Menus {
                     menuEspaciais();
                 } else {
                     System.out.println("Digite uma opção válida.");
+                    continue;
                 }
             }catch (Exception e){
                 System.out.println("Digite uma opção válida.");
@@ -45,7 +46,7 @@ public class Menus {
                 if (opcaoPlana == 1) {
                     System.out.println("\n--------- QUADRADO ---------");
                     System.out.print("Digite o lado do quadrado: ");
-                    double ladoQuadrado = entrada.nextDouble();
+                    double ladoQuadrado = Tratamento.tratamento();
                     Quadrado q1 = new Quadrado(ladoQuadrado);
                     System.out.printf("\nA área do seu quadrado é %.2f\n", q1.calcularArea());
                     System.out.printf("O perímetro do seu quadrado é %.2f\n", q1.calcularPerimetro());
@@ -53,7 +54,7 @@ public class Menus {
                 else if (opcaoPlana == 2) {
                     System.out.println("\n--------- CIRCULO ---------");
                     System.out.print("Digite o raio do circulo: ");
-                    double raioCirculo = entrada.nextDouble();
+                    double raioCirculo = Tratamento.tratamento();
                     Circulo c1 = new Circulo(raioCirculo);
                     System.out.printf("\nA área do seu circulo é %.2f\n", c1.areaCirculo());
                     System.out.printf("O perímetro do seu circulo é %.2f\n", c1.perimetroCirculo());
@@ -61,22 +62,21 @@ public class Menus {
                 else if (opcaoPlana == 3){
                     System.out.println("\n--------- TRIÂNGULO ISÓSC. ---------");
                     System.out.print("Digite a base do seu triângulo: ");
-                    double base = entrada.nextDouble();
+                    double base = Tratamento.tratamento();
                     System.out.print("Digite a altura do seu triângulo: ");
-                    double altura = entrada.nextDouble();
+                    double altura = Tratamento.tratamento();
                     System.out.print("Digite o lado do seu triângulo: ");
-                    double lado = entrada.nextDouble();
+                    double lado = Tratamento.tratamento();
                     Triangulo t1 = new Triangulo(base, altura, lado);
                     System.out.printf("\nA área do seu triângulo é %.2f\n", t1.areaTriangulo());
                     System.out.printf("O perímetro do seu triângulo é %.2f\n", t1.perimetroTriangulo());
-                    System.out.println(t1.altura);
                 }
                 else if (opcaoPlana == 4) {
                     System.out.println("\n--------- RETÂNGULO ---------");
                     System.out.print("Digite a base do retângulo: ");
-                    double baseRetangulo = entrada.nextDouble();
+                    double baseRetangulo = Tratamento.tratamento();
                     System.out.print("Digite a altura do retângulo: ");
-                    double alturaRetangulo = entrada.nextDouble();
+                    double alturaRetangulo = Tratamento.tratamento();
                     Retangulo r1 = new Retangulo(baseRetangulo, alturaRetangulo);
                     System.out.printf("\nA área do seu circulo é %.2f\n", r1.areaRetangulo());
                     System.out.printf("O perímetro do seu circulo é %.2f\n", r1.perimetroRetangulo());
@@ -84,12 +84,14 @@ public class Menus {
                 else if (opcaoPlana == 5){
                     System.out.println("\n--------- Hexágono Regular ---------");
                     System.out.print("Digite o lado do hexágono: ");
-                    double lado = entrada.nextDouble();
+                    double lado = Tratamento.tratamento();
                     Hexagono h1 = new Hexagono(lado);
                     System.out.printf("\nA área do seu Hexágono é %.2f\n", h1.areaHexagono());
                     System.out.printf("O perímetro do seu hexágono é %.2f\n", h1.perimetroHexagono());
-
-
+                }
+                else {
+                    System.out.println("Digite uma opção valida.");
+                    continue;
                 }
             }catch (Exception e){
                 System.out.println("Digite uma opção válida.");
@@ -114,9 +116,9 @@ public class Menus {
                 if (opcaoEspaciais == 1){
                     System.out.println("\n--------- Cubo ---------");
                     System.out.print("Digite o lado do seu cubo: ");
-                    double lado = entrada.nextDouble();
+                    double lado = Tratamento.tratamento();
                     System.out.print("Digite a aresta do seu cubo: ");
-                    double aresta = entrada.nextDouble();
+                    double aresta = Tratamento.tratamento();
                     Cubo c1 = new Cubo(lado, aresta);
                     System.out.printf("\nA área superficial do seu cubo é %.2f\n", c1.calcularAreaS());
                     System.out.printf("O volume do seu cubo é %.2f\n", c1.calcularVol());
@@ -124,11 +126,11 @@ public class Menus {
                 else if (opcaoEspaciais == 2){
                     System.out.println("\n--------- Parelepípedo ---------");
                     System.out.print("Digite o comprimento do seu parelepípedo: ");
-                    double comprimento = entrada.nextDouble();
+                    double comprimento = Tratamento.tratamento();
                     System.out.print("Digite a largura do seu parelepípedo: ");
-                    double largura = entrada.nextDouble();
+                    double largura = Tratamento.tratamento();
                     System.out.print("Digite a altura do seu parelepípedo: ");
-                    double altura = entrada.nextDouble();
+                    double altura = Tratamento.tratamento();
                     Paralelepipedo p1 = new Paralelepipedo(altura, largura, comprimento);
                     System.out.printf("\nA área superficial do seu parelepípedo é %.2f\n", p1.areaSup());
                     System.out.printf("O volume do seu parelepípedo é %.2f\n", p1.volumr());
@@ -137,11 +139,11 @@ public class Menus {
                 else if (opcaoEspaciais == 3){
                     System.out.println("\n--------- Pirâmide de base quadrada ---------");
                     System.out.print("Digite o comprimento da sua pirâmide: ");
-                    double comprimento = entrada.nextDouble();
+                    double comprimento = Tratamento.tratamento();
                     System.out.print("Digite a altura da sua pirâmide: ");
-                    double altura = entrada.nextDouble();
+                    double altura = Tratamento.tratamento();
                     System.out.print("Digite o lado do quadrado da sua pirâmide: ");
-                    double lado = entrada.nextDouble();
+                    double lado = Tratamento.tratamento();
                     Piramide p2 = new Piramide(lado, altura, comprimento);
                     System.out.printf("\nA área superficial da sua pirâmide é %.2f\n", p2.calcularArea());
                     System.out.printf("O volume da da sua pirâmide é %.2f\n", p2.volume());
@@ -149,7 +151,7 @@ public class Menus {
                else if (opcaoEspaciais == 4){
                     System.out.println("\n--------- Esfera ---------");
                     System.out.print("Digite o raio da sua esfera: ");
-                    double raio = entrada.nextDouble();
+                    double raio = Tratamento.tratamento();
                     Esfera e1 = new Esfera(raio);
                     System.out.printf("\nA área superficial da sua esfera é %.2f\n", e1.areaS());
                     System.out.printf("O volume da sua esfera é %.2f\n", e1.volume());
@@ -157,9 +159,9 @@ public class Menus {
                 else if (opcaoEspaciais == 5){
                     System.out.println("\n--------- Cilindro ---------");
                     System.out.print("Digite o raio do circulo de seu cilindro: ");
-                    double raio = entrada.nextDouble();
+                    double raio = Tratamento.tratamento();
                     System.out.print("Digite a altura de seu cilindro: ");
-                    double altura = entrada.nextDouble();
+                    double altura = Tratamento.tratamento();
                     Cilindro c2 = new Cilindro(raio, altura);
                     System.out.printf("\nA área superficial de seu cilindro é %.2f\n", c2.areaS());
                     System.out.printf("O volume de seu cilindro é %.2f\n", c2.volume());
@@ -167,14 +169,19 @@ public class Menus {
                 else if (opcaoEspaciais == 6){
                     System.out.println("\n--------- Cone ---------");
                     System.out.print("Digite o raio do seu cone: ");
-                    double raio = entrada.nextDouble();
+                    double raio = Tratamento.tratamento();
                     System.out.print("Digite a altura do seu cone: ");
-                    double altura = entrada.nextDouble();
+                    double altura = Tratamento.tratamento();
                     Cone c3 = new Cone(altura,raio,0);
                     System.out.printf("\nA área superficial de seu cilindro é %.2f\n", c3.AreaS());
                    System.out.printf("O volume de seu cilindro é %.2f\n", c3.volume());
 
                 }
+                else {
+                    System.out.println("Digite uma opção valida.");
+                    continue;
+                }
+
             }catch (Exception e){
                 System.out.println("Digite uma opção válida.");
                 entrada.nextLine();
@@ -218,3 +225,4 @@ public class Menus {
     }
 
 }
+
