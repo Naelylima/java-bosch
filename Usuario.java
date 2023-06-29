2fe81597fe0f0093d75bc2de1bb3e026181e981b
Naely Lima Pereira <110494299+Naelylima@users.noreply.github.com>
2023-06-29 14:21:21 -0300

    Add files via upload

---
+++ Usuario.java
@@ -0,0 +1,11 @@
+public class Usuario {
+    String nome;
+    String endereco;
+    String cpf;
+
+    public Usuario(String nome, String endereco, String cpf) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.cpf = cpf;
+    }
+}
